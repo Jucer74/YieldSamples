79b5d6e76640699bbc598a006e3f251e6665ff71
Julio Robles <jroblesuribe@deloitte.com>
2022-07-29 16:21:24 -0500

    Update File

---
+++ Code/YieldFile/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+
+
+/*----------*/
+/*-- Main --*/
+/*----------*/
+
+
+Stopwatch stopWatch = new Stopwatch();
+stopWatch.Start();
+Console.WriteLine("Yield Return-Start.");
+ProcessFile("");
+Console.WriteLine("Yield Return-End.");
+stopWatch.Stop();
+TimeSpan ts = stopWatch.Elapsed;
+string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
+Console.WriteLine("RunTime = {0}", elapsedTime);
+
+
+/*---------------*/
+/*-- Functions --*/
+/*---------------*/
+void ProcessFile(string fileName)
+{
+    throw new NotImplementedException();
+}
